e5210fb77730e956b12ba02a10669ee1c1cb5b71
matheuslbrito <matheuslima.b@gmail.com>
2023-06-01 00:55:27 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,6 +1,3 @@
-#!pip install streamli
-#!pip install yfinance
-
 import streamlit as st
 import pandas as pd
 import yfinance as yf
@@ -9,41 +6,42 @@ import matplotlib.pyplot as plt
 from datetime import date
 
 # Página Home
-#def home():
-#    col1, col2 , col3 = st.columns([1,5,1])
-#    with col2:
-#        st.image('myapp_logo.jpg')
-#        st.markdown('---')
-#        st.title('Projeto Final - Matheus Brito')
-#        st.markdown('---')
+def home():
+    col1, col2 , col3 = st.columns([1,5,1])
+    with col2:
+        st.image('myapp_logo.jpg')
+        st.markdown('---')
+        st.title('Projeto Final - Matheus Brito')
+        st.markdown('---')
 
-st.title('Funções de Dados')
+def pairs():
+    st.title('Pairs Trading')
 
+def momentum():
+    st.title('Momentum')
 
-tickers=['PETR4.SA', 'VALE3.SA', 'EQTL3.SA', 'CSAN3.SA']
+def fundamentos():
+    st.title('Fundamentos')
+    tickers=['PETR4.SA', 'VALE3.SA', 'EQTL3.SA', 'CSAN3.SA']
+    precos = yf.download(tickers, period='1y')['Adj Close']
+    st.header('Função st.dataframe()')
+    st.dataframe(precos, width=700, height=300)
 
-precos = yf.download(tickers, period='1y')['Adj Close']
+# Função principal
+def main():
+    st.sidebar.image('myapp_logo.jpg', width=200)
+    st.sidebar.title('Projeto Final')
+    st.sidebar.markdown('---')
+    lista_menu=['Home', 'Pairs Trading', 'Momentum','Fundamentos']
+    escolha = st.sidebar.radio('Escolha a opção', lista_menu)
 
-st.header('Função st.dataframe()')
-st.dataframe(precos, width=700, height=300)
+    if escolha =='Home':
+        home()
+    if escolha == 'Pairs Trading':
+        pairs()
+    if escolha =='Momentum':
+        momentum()
+    if escolha == 'Fundamentos':
+        fundamentos()
 
-st.header('Função st.table()')
-
-st.table(precos.head(10))
-
-st.subheader('Funções st.dataframe e st.table com pandas style')
-
-st.dataframe(precos.head(10).style.highlight_max(axis=1))
-
-st.table(precos.head(10).style.highlight_max(axis=1))
-
-
-st.subheader('Função st.metric()')
-
-st.metric('Temperatura', value='27ºC', delta='1%')
-
-ultimo_petr4 = round(precos['PETR4.SA'].iloc[-1],2)
-penultimo_petr4 = precos['PETR4.SA'].iloc[-2]
-var_petr4 = round(((ultimo_petr4/penultimo_petr4)-1)*100,2)
-
-st.metric('Cotação PETR4', value=ultimo_petr4, delta=f'{var_petr4}%')
+main()
